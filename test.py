129d9e2a7921d8e7e8d35263334d85c42b5982b1
137561160 <61008555+137561160@users.noreply.github.com>
2025-05-22 13:50:25 +0800

    test.py

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
+Looking for a Reliable Sourcing Agent at unbeatable prices in China?
+Hi, I'm Susan — your reliable sourcing agent in China 🇨🇳
+Helping Mexican businesses grow with worry-free sourcing 🇲🇽🤝
+✅ 10+ years of experience
+✅ 300+ happy clients from all over the world
+✅ One-stop sourcing for clothing, electronics, and small commodities etc.
 
+I travel between Shanghai, Guangzhou, Shenzhen, and Yiwu, and I own:
+🏭 A Stamping die factory in Dongguan
+🌏 A foreign trade company in Dongguan
+📦 Multiple warehouses for FREE product storage
+
+Just send me:
+📸 Product pictures | 📏 Size | 🔢 Quantity | Design drawings |
+Or links from 1688, Taobao, Pinduoduo — 
+I'll handle purchasing, quality check, photos/videos & shipping!
+
+Let’s grow your business together. I’m not just your agent, I’m your future friend in China.
+
+Click on the WhatsApp link below
+📞 WhatsApp: https://wa.me/8613424784303
+📧 Email: sales@crazysmall.com
