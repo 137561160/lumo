7d29fd9da6477df4d0d1fc68c5044fef9483f3a1
137561160 <61008555+137561160@users.noreply.github.com>
2025-05-28 11:16:52 +0800

    Update ads.py

--- ads.py
+++ ads.py
@@ -1,11 +1,18 @@
 克罗地亚 7个广告 25.04开始 MU get quote
 伊拉克 没有人
 匈牙利 7个广告 25.04开始 MU get quote
-
+卡塔尔 31个广告 25.03开始 MU 发消息
+印度 4个广告 25.03开始 MU 发消息
 
 
 
 
 对手线
 ---------------------------
-China Guangzhou Foshan Sourcing agent
+China Guangzhou Foshan Sourcing agent  主页点赞是跑的最多的。
+
+
+
+其它
+-------------------------------------------------
+bottle shop 打了很多国家的广告
