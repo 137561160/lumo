18741f0b35c1e5a96a80745abc8970617242cd19
137561160 <61008555+137561160@users.noreply.github.com>
2025-05-28 11:36:01 +0800

    Update ads.py

--- ads.py
+++ ads.py
@@ -3,14 +3,15 @@
 匈牙利 7个广告 25.04开始 MU get quote
 卡塔尔 31个广告 25.03开始 MU 发消息
 印度 4个广告 25.03开始 MU 发消息
-
+厄瓜多尔 22个广告 25.03开始 MU 发消息
 
 
 
 对手线
 ---------------------------
 China Guangzhou Foshan Sourcing agent  主页点赞是跑的最多的。
-
+Jinjie Sourcing Agent
+Home Storage Supplier-UNION HOME
 
 
 其它
