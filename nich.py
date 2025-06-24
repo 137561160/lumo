3569f375d2f4dad925531ee0358c99c80d157257
137561160 <61008555+137561160@users.noreply.github.com>
2025-06-24 15:04:14 +0800

    Update nich.py

--- nich.py
+++ nich.py
@@ -2,4 +2,13 @@ school uniform  UNIFORM catalogue
 door
 floor material
 metal material, sheet metal bar,etc.
+luxury silk products
+Electric Tricycles
+Dinner Plates
+Lenovo 65W laptop charger
+ lithium battery
+solar panel
+---------------------------------------------------------------------
+bed sets
 
+Fellas Loungewear
