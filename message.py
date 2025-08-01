7d511fb5d908e361807086de194bf309b2b22d3a
137561160 <61008555+137561160@users.noreply.github.com>
2025-08-01 14:00:27 +0800

    Update message.py

--- message.py
+++ message.py
@@ -1,1 +1,1 @@
-China peptide factory, we provide high-quality peptides at wholesale prices， oil and tablets available also. Whatsapp: 
+No MOQ, China peptide factory, we provide high-quality peptides at wholesale prices， oil and tablets available also. Whatsapp: https://wa.me/447349366595 tracy@cqlumo.cn
