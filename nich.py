d7b00fc6e919d540142c5c653ac577ed0b3eba28
137561160 <61008555+137561160@users.noreply.github.com>
2026-02-26 14:34:05 +0800

    Add GLP-1 telehealth offers and details
    Added various GLP-1 telehealth offers with details including EPC and CVR.

--- nich.py
+++ nich.py
@@ -23,3 +23,50 @@ Fellas Loungewear
 
 
 ----------------------------------------------------------------
+接上条， ClickDealer 远程医疗offer可以提供证书的有（可以直接上到fb和谷歌，属于白帽的跑法）：
+
+减肥 (176223) [WEB+MOB] ZEVAY - GLP-1 TELEHEALTH - US - CPS  280 usd
+https://zevay.com/product/glp1
+
+减肥 (172664) [WEB+MOB] DirectMeds - GLP-1 Weight Loss Medications /US CPS  $240
+https://directmeds.com/dm-offers-stc/?oid=
+
+减肥 (176838) [WEB+MOB] REFILLS - GLP-1 Weight Loss Made Simple - TELEHEALTH - US - CPS @$280
+http://img.clickdealer-inc.com/mailing/20260205/8c325e85125c01028090b445f39d1bd6.jpg
+EPC: 4% - CVR: 2.20%
+
+减肥 (176837) [WEB+MOB] G-PLANS - GLP-1 MEDICATION - TELEHEALTH - US - CPS @$160
+http://img.clickdealer-inc.com/mailing/20260205/71764d1f107bade44abfd7ad9089a033.jpg
+EPC: $2.50 - CVR; 1.90%
+
+减肥 (176835) [WEB+MOB] IVY Rx - GLP-1 Weight Loss Plans - TELEHEALTH - US - CPS @$215
+http://img.clickdealer-inc.com/mailing/20260205/a9e4f1169f4a412ec844315547e32dd0.jpg
+EPC: $5 - CVR: 2.20%
+
+抗衰 (176836) [WEB+MOB] IVY Rx - NAD+ Injection - TELEHEALTH - US - CPS @$215
+http://img.clickdealer-inc.com/mailing/20260205/d5d9e2fca64d1e04c6e3eb84f2cb5b6b.jpg
+EPC: $4.30 - CVR: 2%
+
+减肥 (176828) [WEB+MOB] FUTURHEALTH - GLP-1 TELEHEALTH - US - CPS @$320
+http://img.clickdealer-inc.com/mailing/20260205/e70fa3c21921b1830c905028d38c7c44.jpg
+EPC: $4 - CVR: 1.50% 
+
+减肥 (176832) [WEB+MOB] FUTURHEALTH - WEGOVY - TELEHEALTH - US - CPS @$140
+http://img.clickdealer-inc.com/mailing/20260205/938a3a8111e5344a7972361a6ee2d4e6.jpg
+EPC: $4 - CVR: 2.10%
+
+男性保健 (176843) [WEB+MOB] TAURUS - TRT - TELEHEALTH - US - CPS @$145
+http://img.clickdealer-inc.com/mailing/20260205/672d5cffd47d7254f181abc9043e5845.jpg
+EPC: $2 - CVR: 2%
+
+男性保健 (177139) [WEB+MOB] TAURUS - 4 in 1 - MALE ENHANCEMENT - TELEHEALTH - US - CPS @115
+http://img.clickdealer-inc.com/mailing/20260213/fd107bec4a5eaaed6441a46280dcc929.jpg
+EPC: $2 - CR 2%
+
+减肥 (176834) [WEB+MOB] YUCCA HEALTH - TELEHEALTH - GLP-1- US - CPS @$280
+http://img.clickdealer-inc.com/mailing/20260205/3d2e2015fb357c56d37e28613615c011.jpg
+EPC: $3.20 - CVR: 1.90%
+
+减肥 (176833) [WEB+MOB] TMATES - Tirzepatide GLP-1 - TELEHEALTH - US - CPS @$265
+http://img.clickdealer-inc.com/mailing/20260205/afafedab77dcdd3a9e374dbdea65eebe.jpg
+EPC: $4 - CVR: 2.20%
